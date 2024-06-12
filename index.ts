6e98bb29546b0e0d6750a5db40dbb3f779cb046b
GregoireARichard <gregoire.richard@hetic.net>
2024-06-12 13:52:09 +0200

    header

--- index.ts
+++ index.ts
@@ -11,16 +11,17 @@ dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
+const appLink = process.env.APP_LINK ? process.env.APP_LINK : "main--toast-peanutbot.netlify.app"
 
 app.use(express.json());
 app.use(cors({
-  origin: process.env.APP_LINK,
+  origin: appLink,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 const cronExpression = "*/15 * * * * *";
-const appLink = process.env.APP_LINK ? process.env.APP_LINK : "main--toast-peanutbot.netlify.app"
+
 const cronJob = new cron.CronJob(cronExpression, Job.placeOrder);
 //cronJob.start();
 
@@ -28,12 +29,7 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
 //admin
-app.post("/admin/login", (req: Request, res: Response) => {
-  res.setHeader('Access-Control-Allow-Origin', appLink);
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  AdminController.login(req, res);
-});
+app.post("/admin/login", AdminController.login);
 // app.post("/admin/signup", AdminController.signup);
 
 // front controls
